9201783e2ed6c30affe080ed928560f0940fbcfe
Dennys Antunish <dantunish@nyc.yearup.org>
2018-11-07 15:01:01 -0500

    rough draft

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
 console.log("An Army has Risen")
+  document.getElementById('buton_1').addEventListener('mouseover',tea);
+function tea(){
+    let x=document.createElement("img");
+  x.setAttribute("src","buy.jpg");
+  x.setAttribute("width","150px");
+  x.setAttribute("height", "200px");
+  document.getElementById('div_pic').appendChild(x);
+
+}
+
+
+
+
+function lol(){
+  //changes the inner html of the h1
+let x=document.getElementsByClassName("h1_company")[0];
+      x.innerHTML="Im currently selling this website so if you found this please call 555-555-5556. I need money and this company didn't give it.";
+      x.style.textShadow = "2px 4px yellow";
+      x.style.width = "600px";
+      x.style.verticalAlign="middle";
+
+      if (x.innerHTML="Im currently selling this website so if you found this please call 555-555-5556. I need money and this company didn't give it."){
+        document.getElementsByClassName('h1_company').addEventListener
+      }
+
+        // adds a new element and attribute
+    let y = document.createElement("img");
+
+    y.setAttribute("src", "dollar.jpg");
+    y.style.width="1250px";
+      document.getElementsByClassName('div_h1')[0].appendChild(y);
+}
